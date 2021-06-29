50705a586e2b2ee17e5f89695d77d520921290d5
khushal <totakhailkhushal@gmail.com>
2021-06-29 16:47:39 +0200

    Task Solved

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
-# Submit me 
+# Submit me
 
-**Instructions**: 
+**Instructions**:
 
-* Look at the form on HTML. In the `index.js` file, create a post request on submit. 
+- Look at the form on HTML. In the `index.js` file, create a post request on submit.
 
-**Requirements**: 
-* Use fetch to post data. 
-* Use `async await` syntax.
-* When resolving your promise, print the raw JSON to the console. 
-* Alert the user once they have submitted the form.
+**Requirements**:
 
-* You can use [JSONPlaceholder](https://jsonplaceholder.typicode.com/) to make a POST request. 
+- Use fetch to post data.
+- Use `async await` syntax.
+- When resolving your promise, print the raw JSON to the console.
+- Alert the user once they have submitted the form.
+
+- You can use [JSONPlaceholder](https://jsonplaceholder.typicode.com/) to make a POST request.
 
 ![alt text](image/reference.png "On Submission")
+
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,69 @@ import "babel-polyfill";
 
 // \/ All of your javascript should go here \/
 
+// const form =document.querySelector("#submit-form")
+
+// form.addEventListener("submit",async (e) =>{
+//     e.preventDefault();
+    
+//     const user={
+//         name:name.value,
+//         email:email.value,
+//         password:password.value,
+//         message:message.value,
+//         checkbox:checkbox.checked,
+//     }
+//     console.log(user)
+
+//      await fetch("https://jsonplaceholder.typicode.com/users" ,{
+//         method:"POST",
+//         body:JSON.stringify(user)
+//     })
+//     .then(response => response.json())
+//     .then(result => {
+//         console.log(result)
+//         alert("Thank you for submitting your detail, User Id:4")
+        
+
+//     }) 
+//     .catch(e => console.warn("woops woops "))
+// })
+
+
+    // =====================================================
+
+const form =document.querySelector("#submit-form")
+
+ const getUser = async(user) => {
+    try{
+    const fetchData= await fetch("https://jsonplaceholder.typicode.com/users" ,{
+        method:"POST",
+        body:JSON.stringify(user)
+    })
+    alert("Thank you for submitting your detail, User Id:4")
+    
+        const changeToJson = await fetchData.json()
+        return changeToJson;
+        console.log(user);
+        
+    }catch{return "error"}
+    }
+ 
+
+form.addEventListener("submit",(e) =>{
+    e.preventDefault();
+    
+    const user={
+        name:name.value,
+        email:email.value,
+        password:password.value,
+        message:message.value,
+        checkbox:checkbox.checked,
+    }
+
+    getUser(user)
+    .then(response => console.log(response))
+}).catch(console.log("Error 2"))
+// console.log(user);
+
+   
